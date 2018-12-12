c9d6d131d0e7d0231e5b6d774d627c14aa0cbd22
luke <luke.bogacz@basket.com>
2018-12-12 09:56:37 -0500

    final test

--- semver.sh
+++ semver.sh
@@ -13,21 +13,21 @@ if [[ ${PATCH_TYPE} == *"patch"* ]] || [[ ${PATCH_TYPE} == *"minor"* ]] || [[ ${
     echo -e "\n - Current git tag version: $(git describe --tags)";
     RELEASE_BRANCH="release/${NEW_VERSION/v/}";
 
-#    echo -e "\n - Updating(${PATCH_TYPE}) version to ${NEW_VERSION} ";
-#    git checkout -b ${RELEASE_BRANCH};
+    echo -e "\n - Updating(${PATCH_TYPE}) version to ${NEW_VERSION} ";
+    git checkout -b ${RELEASE_BRANCH};
 #
     echo -e "\n - Cutting ${RELEASE_BRANCH} branch";
-#    git commit -m "Bump version to ${NEW_VERSION}";
-#    git push -u origin ${RELEASE_BRANCH};
-#
-#    echo -e "\n - Tagging commits and requesting PR to master";
-#    git pull origin ${RELEASE_BRANCH};
-#    git push origin ${NEW_VERSION};
-#
-#    echo -e "\n - Merging ${RELEASE_BRANCH} into develop";
-#    git checkout develop;
-#    git merge master;
-#    git push origin develop;
+    git commit -m "Bump version to ${NEW_VERSION}";
+    git push -u origin ${RELEASE_BRANCH};
+
+    echo -e "\n - Tagging commits and requesting PR to master";
+    git pull origin ${RELEASE_BRANCH};
+    git push origin ${NEW_VERSION};
+
+    echo -e "\n - Merging ${RELEASE_BRANCH} into develop";
+    git checkout develop;
+    git merge master;
+    git push origin develop;
 
     echo -e "\n - Done";
 else
