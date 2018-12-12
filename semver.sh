eea9c8bdd85d1eb6ceade370eb1a907ebb21883f
luke <luke.bogacz@basket.com>
2018-12-12 08:22:21 -0500

    initial commit

---
+++ semver.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+PATCH_TYPE=${1};
+
+if [[ ${PATCH_TYPE} == *"patch"* ]] || [[ ${PATCH_TYPE} == *"minor"* ]] || [[ ${PATCH_TYPE} == *"major"* ]]; then
+    echo -e "\n - Starting release process for ${PATCH_TYPE}";
+    git checkout develop && cd static
+
+    VERSION=$(npm version ${PATCH_TYPE});
+    cd ..
+
+    echo -e "\n - Current git tag w/ commit: $(git describe --tags)";
+    echo -e "\n - Updating(${PATCH_TYPE}) version to ${VERSION}";
+    RELEASE_BRANCH="release/${VERSION}";
+
+    git tag -a ${VERSION} -m "Bump version to ${VERSION}"
+    git checkout -b ${RELEASE_BRANCH};
+
+    echo -e "\n - Committing ${RELEASE_BRANCH} to git";
+    git add . && git commit -a -m "Bump version to ${VERSION}" && git push -u origin ${RELEASE_BRANCH};
+    git request-pull origin/master ${RELEASE_BRANCH};
+
+    echo -e "\n - Merging ${RELEASE_BRANCH} into develop";
+    git checkout develop;
+    git merge ${RELEASE_BRANCH};
+    git add . && git commit -a -m "${VERSION}" && git push origin develop;
+
+    echo -e "\n - Done";
+else
+    echo "ERROR: You must provide an npm patch type (patch, minor, major)";
+fi;
