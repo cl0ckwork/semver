224821775abc4d3e6555e258d43687772c57dec2
luke <luke.bogacz@basket.com>
2018-12-12 08:57:57 -0500

    Bump version to

--- semver.sh
+++ semver.sh
@@ -12,20 +12,21 @@ if [[ ${PATCH_TYPE} == *"patch"* ]] || [[ ${PATCH_TYPE} == *"minor"* ]] || [[ ${
     echo -e "\n - Current git tag w/ commit: $(git describe --tags)";
     RELEASE_BRANCH="release/${VERSION}";
 
-    echo -e "\n - Updating(${PATCH_TYPE}) version to ${VERSION} and cutting ${RELEASE_BRANCH} branch";
+    echo -e "\n - Updating(${PATCH_TYPE}) version to ${VERSION} ";
     git checkout -b ${RELEASE_BRANCH};
 
-    echo -e "\n - Committing ${RELEASE_BRANCH} to git";
+    echo -e "\n - Cutting ${RELEASE_BRANCH} branch";
     git commit -a -m "Bump version to ${VERSION}" && git push -u origin ${RELEASE_BRANCH};
 
+    echo -e "\n - Tagging commits and requesting PR to master";
     git tag -a ${VERSION} -m "Bump version to ${VERSION}" --force;
     git push origin ${VERSION};
     git request-pull origin/master ${RELEASE_BRANCH};
 
     echo -e "\n - Merging ${RELEASE_BRANCH} into develop";
-#    git checkout develop;
-#    git merge master
-#    git push origin develop;
+    git checkout develop;
+    git merge master
+    git push origin develop;
 
     echo -e "\n - Done";
 else
