3f2c54a2e17cd1629c2c961e352a937273d62654
luke <luke.bogacz@basket.com>
2018-12-12 08:56:03 -0500

    new

--- semver.sh
+++ semver.sh
@@ -16,7 +16,7 @@ if [[ ${PATCH_TYPE} == *"patch"* ]] || [[ ${PATCH_TYPE} == *"minor"* ]] || [[ ${
     git checkout -b ${RELEASE_BRANCH};
 
     echo -e "\n - Committing ${RELEASE_BRANCH} to git";
-    git add . && git commit -a -m "Bump version to ${VERSION}" && git push -u origin ${RELEASE_BRANCH};
+    git commit -a -m "Bump version to ${VERSION}" && git push -u origin ${RELEASE_BRANCH};
 
     git tag -a ${VERSION} -m "Bump version to ${VERSION}" --force;
     git push origin ${VERSION};
